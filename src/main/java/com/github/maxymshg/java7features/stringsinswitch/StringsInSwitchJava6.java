536d605c74fa19fe1b3afad89002a0fce8ab27eb
Maxim Shekhovets <maxymshg@gmail.com>
2013-03-26 00:00:29 +0200

    - basic settings for Netbeans SBT support; - strings in switch test case.

---
+++ src/main/java/com/github/maxymshg/java7features/stringsinswitch/StringsInSwitchJava6.java
@@ -0,0 +1,8 @@
+package com.github.maxymshg.java7features.stringsinswitch;
+
+public class StringsInSwitchJava6 {
+
+  public static void main(String[] args) {
+    System.out.println("Strings in switch are not supported by Java 6 and earlier.");
+  }
+}
---
+++ src/main/java/com/github/maxymshg/java7features/stringsinswitch/StringsInSwitchJava7.java
@@ -0,0 +1,53 @@
+package com.github.maxymshg.java7features.stringsinswitch;
+
+public class StringsInSwitchJava7 {
+
+  enum Planet {
+    MERCURY,
+    VENUS,
+    EARTH,
+    MARS,
+    JUPITER,
+    SATURN,
+    URANUS,
+    NEPTUNE;
+  }
+
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.err.println("Usage: java {Planet name}");
+      System.exit(-1);
+    }
+    String name = args[0].toUpperCase();
+    switch (name) {
+      case "MERCURY":
+        System.out.println("You have been transfered on: " + Planet.MERCURY);
+        break;
+      case "VENUS":
+        System.out.println("You have been transfered on: " + Planet.VENUS);
+        break;
+      case "EARTH":
+        System.out.println("You have been transfered on: " + Planet.EARTH);
+        break;
+      case "MARS":
+        System.out.println("You have been transfered on: " + Planet.MARS);
+        break;
+      case "JUPITER":
+        System.out.println("You have been transfered on: " + Planet.JUPITER);
+        break;
+      case "SATURN":
+        System.out.println("You have been transfered on: " + Planet.SATURN);
+        break;
+      case "URANUS":
+        System.out.println("You have been transfered on: " + Planet.URANUS);
+        break;
+      case "NEPTUNE":
+        System.out.println("You have been transfered on: " + Planet.NEPTUNE);
+        break;
+      default:
+        System.out.println("Transporter is not working.");
+        break;
+    }
+
+  }
+}
